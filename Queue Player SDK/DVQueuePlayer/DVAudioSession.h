a40af2eae610398a07cc1c9f9f5a7a3899a67e4a
Mikhail Grushin <grushin@denivip.ru>
2013-06-07 16:55:59 +0400

    added audio session class

---
+++ Queue Player SDK/DVQueuePlayer/DVAudioSession.h
@@ -0,0 +1,18 @@
+//
+//  DVAudioSession.h
+//  Queue Player SDK
+//
+//  Created by Mikhail Grushin on 07.06.13.
+//  Copyright (c) 2013 DENIVIP Group. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DVAudioSession : NSObject
+
++ (instancetype)defaultSession;
+@property (nonatomic, assign, getter = isMuted) BOOL muted;
+@property (nonatomic, assign) float volume;
+@property (nonatomic, strong) NSError *error;
+
+@end
