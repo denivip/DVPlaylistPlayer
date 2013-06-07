ee0b635c9b6eb188ed63aa1cd7034a2d8ae8c870
Mikhail Grushin <grushin@denivip.ru>
2013-06-07 13:11:37 +0400

    created project framework

---
+++ Queue Player SDK/DVQueuePlayer/DVQueuePlayer.m
@@ -0,0 +1,17 @@
+//
+//  DVQueuePlayer.m
+//  Queue Player SDK
+//
+//  Created by Mikhail Grushin on 07.06.13.
+//  Copyright (c) 2013 DENIVIP Group. All rights reserved.
+//
+
+#import "DVQueuePlayer.h"
+
+@implementation DVQueuePlayer
+
++ (void)SayHello {
+    NSLog(@"Hello from the framework!");
+}
+
+@end
---
+++ Queue Player SDK/DVQueuePlayerSDK.h
@@ -0,0 +1,9 @@
+//
+//  DVQueuePlayer.h
+//  Queue Player SDK
+//
+//  Created by Mikhail Grushin on 07.06.13.
+//  Copyright (c) 2013 DENIVIP Group. All rights reserved.
+//
+
+#import "DVQueuePlayer.h"
